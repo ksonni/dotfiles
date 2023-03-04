e9945afcb88fc32ca23360e1539dd3bacb28b359
Keshav Sonni <keshav.vm97@gmail.com>
2023-03-04 09:55:13 +0000

    Setup treesitter

--- init.lua
+++ init.lua
@@ -1,11 +1,17 @@
-user = "default"
+user = "default."
 
-require(user .. ".plugins-setup")
-require(user .. ".core.options")
-require(user .. ".core.keymaps")
-require(user .. ".core.colorscheme")
-require(user .. ".plugins.comment")
-require(user .. ".plugins.nvim-tree")
-require(user .. ".plugins.lualine")
-require(user .. ".plugins.telescope")
-require(user .. ".plugins.nvim-cmp")
+require(user .. "plugins-setup")
+require(user .. "core.options")
+require(user .. "core.keymaps")
+require(user .. "core.colorscheme")
+require(user .. "plugins.comment")
+require(user .. "plugins.nvim-tree")
+require(user .. "plugins.lualine")
+require(user .. "plugins.telescope")
+require(user .. "plugins.nvim-cmp")
+require(user .. "plugins.lsp.mason")
+require(user .. "plugins.lsp.lspsaga")
+require(user .. "plugins.lsp.lspconfig")
+require(user .. "plugins.autopairs")
+require(user .. "plugins.treesitter")
+
--- lua/default/plugins-setup.lua
+++ lua/default/plugins-setup.lua
@@ -59,6 +59,30 @@ return packer.startup(function(use)
   use("L3MON4D3/LuaSnip") -- snippet engine
   use("saadparwaiz1/cmp_luasnip") -- for autocompletion
   use("rafamadriz/friendly-snippets") -- useful snippets
+  
+  -- managing & installing lsp servers, linters & formatters
+  use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
+  use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
+  
+  -- configuring lsp servers
+  use("neovim/nvim-lspconfig") -- easily configure language servers
+  use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
+  use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
+  use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
+  use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+  
+  -- treesitter configuration
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = function()
+      local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+      ts_update()
+    end,
+  })
+
+  -- auto closing
+  use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
+  use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
 
   if packer_bootstrap then
     require("packer").sync()

ec541f146b4ca53f3923f6e5641d3df39726e10a
Keshav Sonni <keshav.vm97@gmail.com>
2023-03-04 08:03:47 +0000

    Setup telescope

--- lua/default/plugins-setup.lua
+++ lua/default/plugins-setup.lua
@@ -45,6 +45,10 @@ return packer.startup(function(use)
   use("nvim-tree/nvim-web-devicons") -- vs-code like icons
  
   use("nvim-lualine/lualine.nvim") -- status line
+   
+  -- fuzzy finding w/ telescope
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
 
   if packer_bootstrap then
     require("packer").sync()

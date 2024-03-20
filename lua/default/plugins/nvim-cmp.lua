1933e9d51ffa2addb49f6b8199d33e711e897b79
Keshav Sonni <keshav.vm97@gmail.com>
2024-03-20 22:14:01 +0000

    Word wrap and markdown

--- lua/default/plugins/nvim-cmp.lua
+++ lua/default/plugins/nvim-cmp.lua
@@ -18,6 +18,7 @@ end
 
 -- load vs-code like snippets from plugins (e.g. friendly-snippets)
 require("luasnip/loaders/from_vscode").lazy_load()
+require("luasnip/loaders/from_vscode").lazy_load { paths = "./snippets/markdown" }
 
 cmp.setup({
   snippet = {

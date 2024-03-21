4f5518e949d76af9a2f18ddfc948fb11bb24a132
Keshav Sonni <keshav.vm97@gmail.com>
2024-03-21 21:48:12 +0000

    setup treesitter

--- lua/primeagen/packer.lua
+++ lua/primeagen/packer.lua
@@ -4,11 +4,13 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  -- file finder
   use {
       'nvim-telescope/telescope.nvim', tag = '0.1.4', 
       requires = { {'nvim-lua/plenary.nvim'} }
   }
-
+	
+  -- color scheme
   use({
       'rose-pine/neovim',
       as = 'rose-pine',
@@ -16,6 +18,15 @@ return require('packer').startup(function(use)
 	  vim.cmd('colorscheme rose-pine')
       end
   })
+
+  -- syntax highlighting
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+	ts_update()
+    end
+  }
 
 end)
 

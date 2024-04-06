a2ca5432c1bdd420b201ad0aaf8221dd0bece804
Keshav Sonni <keshav.vm97@gmail.com>
2024-04-06 23:59:26 +0100

    change telescope layout to vertical

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,6 @@
 local builtin = require('telescope.builtin')
+local telescope = require('telescope')
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
@@ -6,3 +8,8 @@ vim.keymap.set('n', '<leader>ps', function()
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 
+telescope.setup({
+    defaults = {
+        layout_strategy = 'vertical'
+    },
+})

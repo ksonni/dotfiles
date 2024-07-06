dc8afb38ab3d02f4666c8096f4911455a0b2074a
Keshav Sonni <keshav.vm97@gmail.com>
2024-07-06 07:16:30 +0100

    harpoon: larger pane

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,5 +1,6 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
+local harpoon = require("harpoon")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu)
@@ -8,3 +9,10 @@ vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
+
+harpoon.setup({
+    menu = {
+        width = 128,
+    },
+})
+

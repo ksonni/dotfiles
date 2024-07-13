0eb9199fa5209fdd984db9e0dc1dd1bd41fe974d
Keshav Sonni <keshav.vm97@gmail.com>
2024-07-13 04:57:39 +0100

    fugitive: improve git diff view

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,3 +3,6 @@ local dracula = require('dracula')
 dracula.setup({})
 
 vim.cmd.colorscheme('dracula')
+
+-- Tweak the fugitive diff view
+vim.cmd [[highlight DiffAdd ctermbg=237 guifg=NONE guibg=#2c452a]]

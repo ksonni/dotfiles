f2bca6502519773ea7067208663a55884507f670
Keshav Sonni <keshav.vm97@gmail.com>
2024-10-16 00:24:44 +0100

    scripts: add a script to easily spawn tmux sessions from a directory

---
+++ tm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Starts a session named after the current directory
+# If the name of a session is provided, it attaches to it
+
+SESSION_NAME=${1:-$(basename "$PWD")}
+
+if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+    tmux attach-session -t "$SESSION_NAME"
+else
+    tmux new-session -d -s "$SESSION_NAME" -n editor "nvim ."
+    tmux new-window -t "$SESSION_NAME"
+    tmux select-window -t "$SESSION_NAME:0"
+    tmux attach-session -t "$SESSION_NAME"
+fi
+

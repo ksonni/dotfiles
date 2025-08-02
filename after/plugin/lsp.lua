65a3606cba4fd27c565481d620093c3f42ff5b55
Keshav Sonni <keshav.vm97@gmail.com>
2025-08-02 19:04:49 +0100

    lsp: some remaps for renaming etc.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -59,10 +59,10 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "<leader>n", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "<leader>p", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
 
     vim.keymap.set("n", "<leader>o", function()
       vim.lsp.buf.code_action({
@@ -74,8 +74,8 @@ lsp.on_attach(function(client, bufnr)
       })
     end, opts)
 
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    -- vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 

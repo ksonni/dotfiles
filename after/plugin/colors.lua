cf4f5472e7acaf9b0f146901affb6c40280b51b0
Keshav Sonni <keshav.vm97@gmail.com>
2025-09-20 00:46:52 +0100

    theme: align colors of tmux and neovim separators

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -6,10 +6,13 @@ end
 dracula.setup({
     colors = {
         bg = '#1E1F22',
-    }
+    },
+    transparent_bg = true,
 })
 
 vim.cmd.colorscheme('dracula')
 
 -- Tweak the fugitive diff view
 vim.cmd [[highlight DiffAdd ctermbg=237 guifg=NONE guibg=#2c452a]]
+
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#323438", bg = "NONE" })

f3d895987e9c686dd6951462810c27ed7a84c6be
Keshav Sonni <keshav.vm97@gmail.com>
2025-09-24 22:47:34 +0100

    telescope: decode file_ignore_patterns from VIMIGNORE env variable

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -11,8 +11,19 @@ vim.keymap.set('n', '<leader>ps', function()
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 
+-- Use an env variables to ignore certain directories in the project
+-- without cluttering the repo with a specific .ignore file.
+-- in your telescope setup file
+local ignore_env = vim.env.VIMIGNORE or ""
+local ignore_patterns = {}
+for pat in string.gmatch(ignore_env, '([^,]+)') do
+    pat = pat:gsub("^%s+", ""):gsub("%s+$", "") -- trim spaces
+    table.insert(ignore_patterns, pat)
+end
+
 telescope.setup({
     defaults = {
-        layout_strategy = 'vertical'
+        layout_strategy = 'vertical',
+        file_ignore_patterns = ignore_patterns,
     },
 })

daa34d233746a818f60fe44b9f645a9cf9ff0f49
Keshav Sonni <keshav.vm97@gmail.com>
2026-02-25 04:08:10 +0000

    colors: border colors to better fit with transulcense

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -20,4 +20,4 @@ vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "none" })
 -- Tweak the fugitive diff view
 vim.cmd [[highlight DiffAdd ctermbg=237 guifg=NONE guibg=#2c452a]]
 
-vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#323438", bg = "NONE" })
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#626363", bg = "NONE" })

acd82d02487d27714374aefb47124b84f2f8d857
Keshav Sonni <keshav.vm97@gmail.com>
2026-02-26 02:05:53 +0000

    remap: remaps to yank file/line location info for agents

--- lua/default/remap.lua
+++ lua/default/remap.lua
@@ -52,3 +52,44 @@ vim.keymap.set('n', '<C-w><Right>', '<cmd>vertical resize +10<CR>')
 vim.keymap.set('n', '<C-w><Left>', '<cmd>vertical resize -10<CR>')
 vim.keymap.set('n', '<C-w><Up>', '<cmd>resize +5<CR>')
 vim.keymap.set('n', '<C-w><Down>', '<cmd>resize -5<CR>')
+
+-- Copy filename for copilot
+vim.keymap.set("n", "<leader>ay", function()
+    vim.fn.setreg("+", "@" .. vim.fn.expand("%:."))
+end, { desc = "Copy relative file path" })
+
+-- Yank relative file name for AI agents
+vim.keymap.set("n", "<leader>af", function()
+    vim.fn.setreg("+", "@" .. vim.fn.expand("%:."))
+end, { desc = "Yank @file (relative path)" })
+
+-- Yank relative file name with line number
+vim.keymap.set("n", "<leader>al", function()
+    local path = vim.fn.expand("%:.")
+    local line = vim.fn.line(".")
+    vim.fn.setreg("+", "@" .. path .. ":" .. line)
+end, { desc = "Yank @file:line" })
+
+-- Yank relative file name with line range and exit viual mode
+vim.keymap.set("v", "al", function()
+    local path = vim.fn.expand("%:.")
+
+    local anchor = vim.fn.getpos("v") -- start of selection
+    local cursor = vim.fn.getpos(".") -- end of selection (current cursor)
+
+    local a_line, a_col = anchor[2], anchor[3]
+    local c_line, c_col = cursor[2], cursor[3]
+
+    local start_line, end_line = a_line, c_line
+    if start_line > end_line then start_line, end_line = end_line, start_line end
+
+    vim.fn.setreg("+", ("@%s:%d-%d"):format(path, start_line, end_line))
+
+    -- exit visual mode
+    vim.cmd("normal! \27")
+
+    -- mimic `y` cursor behavior:
+    if (a_line < c_line) or (a_line == c_line and a_col <= c_col) then
+        vim.fn.setpos(".", anchor)
+    end
+end, { desc = "Yank @file:start-end" })
